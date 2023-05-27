e2d41add03a2e406b068c875aa967430b67439d8
Gikon3 <13802189+Gikon3@users.noreply.github.com>
2023-05-28 01:14:11 +0300

    add: итераторы; mod: изменена реализация на вектор векторов

--- Matrix.h
+++ Matrix.h
@@ -1,128 +1,116 @@
 #pragma once
-#include <ostream>
-#include <vector>
-#include <initializer_list>
 
-template<typename T = int>
+#include <initializer_list>
+#include <vector>
+#include <iterator>
+
+template<typename T>
 class Matrix final
 {
-public:
-    class Row
+    template<typename R, typename Fr>
+    class ItBasic final
     {
+        friend Fr;
     public:
-        Row(std::vector<T>::iterator it);
-        T& operator[](int i);
+        using value_type = std::vector<R>::iterator::value_type;
+        using difference_type = std::vector<R>::iterator::difference_type;
+        using pointer = std::vector<R>::iterator::pointer;
+        using reference = std::vector<R>::iterator::reference;
+
+    public:
+        ItBasic() = default;
+        R& operator*() { return *itImpl_; }
+        const R& operator*() const { return *itImpl_; }
+        bool operator==(const ItBasic& rhs) const { return itImpl_ == rhs.itImpl_; }
+        bool operator!=(const ItBasic& rhs) const { return itImpl_ != rhs.itImpl_; }
+        ItBasic& operator++() {
+            ++itImpl_;
+            return *this;
+        }
+        ItBasic& operator++(int) {
+            ItBasic temp(itImpl_++);
+            return temp;
+        }
 
     private:
-        std::vector<T>::iterator it_;
-    };
-
-    class CRow
-    {
-    public:
-        CRow(std::vector<T>::const_iterator it);
-        const T& operator[](int i) const;
+        ItBasic(std::vector<R>::iterator it) : itImpl_(it) {}
 
     private:
-        std::vector<T>::const_iterator it_;
+        std::vector<R>::iterator itImpl_;
+    };
+
+    template<typename R, typename Fr>
+    class CItBasic final
+    {
+        friend Fr;
+    public:
+        using value_type = std::vector<R>::const_iterator::value_type;
+        using difference_type = std::vector<R>::const_iterator::difference_type;
+        using pointer = std::vector<R>::const_iterator::pointer;
+        using reference = std::vector<R>::const_iterator::reference;
+
+    public:
+        CItBasic() = default;
+        const R& operator*() const { return *itImpl_; }
+        bool operator==(const CItBasic& rhs) const { return itImpl_ == rhs.itImpl_; }
+        bool operator!=(const CItBasic& rhs) const { return itImpl_ != rhs.itImpl_; }
+        CItBasic& operator++() {
+            ++itImpl_;
+            return *this;
+        }
+        CItBasic& operator++(int) {
+            CItBasic temp(itImpl_++);
+            return temp;
+        }
+
+    private:
+        CItBasic(std::vector<R>::const_iterator it) : itImpl_(it) {}
+
+    private:
+        std::vector<R>::const_iterator itImpl_;
     };
 
 public:
-    Matrix(int nRows, int nColumns);
-    Matrix(std::initializer_list<std::initializer_list<T>> list);
-    int nrows() const;
-    int ncols() const;
-    Row operator[](int j) &;
-    CRow operator[](int j) const &;
-    void dump(std::ostream& os) const;
+    class Row final
+    {
+    public:
+        using iterator = ItBasic<T, Row>;
+        using const_iterator = CItBasic<T, Row>;
+
+    public:
+        Row(std::size_t nColumns) : data_(nColumns) {}
+        Row(std::initializer_list<T> ilist) : data_(ilist) {}
+        T& operator[](std::size_t i) { return data_[i]; }
+        const T& operator[](std::size_t i) const { return data_[i]; }
+        iterator begin() { return iterator(data_.begin()); }
+        iterator end() { return iterator(data_.end()); }
+        const_iterator begin() const { return const_iterator(data_.begin()); }
+        const_iterator end() const { return const_iterator(data_.end()); }
+        const_iterator cbegin() const { return const_iterator(data_.cbegin()); }
+        const_iterator cend() const { return const_iterator(data_.cend()); }
+
+    private:
+        std::vector<T> data_;
+    };
+
+    using iterator = ItBasic<Row, Matrix>;
+    using const_iterator = CItBasic<Row, Matrix>;
+
+public:
+    Matrix(std::size_t nRows, std::size_t nColumns) : rows_(nRows, Row(nColumns)) {}
+    Matrix(std::initializer_list<std::initializer_list<T>> ilist) {
+        for (const auto& irow : ilist)
+            rows_.emplace_back(irow);
+    }
+    Row& operator[](std::size_t j) { return rows_[j]; }
+    const Row& operator[](std::size_t j) const { return rows_[j]; }
+    iterator begin() { return iterator(rows_.begin()); }
+    iterator end() { return iterator(rows_.end()); }
+    const_iterator begin() const { return const_iterator(rows_.begin()); }
+    const_iterator end() const { return const_iterator(rows_.end()); }
+    const_iterator cbegin() const { return const_iterator(rows_.cbegin()); }
+    const_iterator cend() const { return const_iterator(rows_.cend()); }
 
 private:
-    std::vector<T> data_;
-    const int nColumns_;
+    std::vector<Row> rows_;
 };
-
-// ############################################################################
-template<typename T>
-Matrix<T>::Matrix(int nRows, int nColumns) :
-    data_(nRows * nColumns), nColumns_(nColumns)
-{
-}
-
-template<typename T>
-Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T>> ilist) :
-    nColumns_(static_cast<int>((*ilist.begin()).size()))
-{
-    for (auto& row : ilist)
-        data_.insert(data_.cend(), row);
-}
-
-template<typename T>
-int Matrix<T>::nrows() const
-{
-    return data_.size() / nColumns_;
-}
-
-template<typename T>
-int Matrix<T>::ncols() const
-{
-    return nColumns_;
-}
-
-template<typename T>
-Matrix<T>::Row Matrix<T>::operator[](int j) &
-{
-    return Row(data_.begin() + j * nColumns_);
-}
-
-template<typename T>
-Matrix<T>::CRow Matrix<T>::operator[](int j) const &
-{
-    return CRow(data_.begin() + j * nColumns_);
-}
-
-template<typename T>
-void Matrix<T>::dump(std::ostream& os) const
-{
-    const int nRows = data_.size() / nColumns_;
-    for (int i = 0; i < data_.size(); ++i) {
-        os << data_[i];
-        if ((i + 1) % nRows == 0)
-            os << std::endl;
-        else
-            os << "\t";
-    }
-}
-
-// ############################################################################
-template<typename T>
-Matrix<T>::Row::Row(std::vector<T>::iterator it) :
-    it_(it)
-{
-}
-
-template<typename T>
-T& Matrix<T>::Row::operator[](int i)
-{
-    return *(it_ + i);
-}
-
-// ############################################################################
-template<typename T>
-Matrix<T>::CRow::CRow(std::vector<T>::const_iterator it) :
-    it_(it)
-{
-}
-
-template<typename T>
-const T& Matrix<T>::CRow::operator[](int i) const
-{
-    return *(it_ + i);
-}
-
-// ############################################################################
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const Matrix<T>& m)
-{
-    m.dump(os);
-    return os;
-}
